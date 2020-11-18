cf6143609a6523bbe86172e6735b59a409d4bd4f
Obilarius <git@walzenbach.com>
2020-11-18 11:40:53 +0100

    react init

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-const { response } = require("express");
-const app = require("express")();
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-app.get("/q", (req, res) => res.sendFile(__dirname + "/admin/questions.html"));
-app.get("/api/getQuestions", (req, res) =>
-  res.sendFile(__dirname + "/questions.json")
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-app.listen(9091, () => console.log("Listening on http port 9091"));
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
